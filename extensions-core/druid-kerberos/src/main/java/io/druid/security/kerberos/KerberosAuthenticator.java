4bcf242f0aa0204f13eb9c51fa40c71b6103c45a
Ferrot Patrice, INI-DNA-DL <patrice.ferrot@swisscom.com>
2019-01-23 15:03:22 +0100

    Changes to build with the HDP 2.6.4 Hadoop version (2.7.3.2.6.4.0-91)

--- extensions-core/druid-kerberos/src/main/java/io/druid/security/kerberos/KerberosAuthenticator.java
+++ extensions-core/druid-kerberos/src/main/java/io/druid/security/kerberos/KerberosAuthenticator.java
@@ -346,7 +346,9 @@ public class KerberosAuthenticator implements Authenticator
               if (newToken && !token.isExpired() && token != AuthenticationToken.ANONYMOUS) {
                 String signedToken = mySigner.sign(token.toString());
                 createAuthCookie(httpResponse, signedToken, getCookieDomain(),
-                                 getCookiePath(), token.getExpires(), isHttps
+                                 getCookiePath(), token.getExpires(),
+                                 !token.isExpired() && token.getExpires() > 0,
+                                 isHttps
                 );
                 request.setAttribute(SIGNED_TOKEN_ATTRIBUTE, tokenToCookieString(
                     signedToken,
@@ -381,7 +383,7 @@ public class KerberosAuthenticator implements Authenticator
         if (unauthorizedResponse) {
           if (!httpResponse.isCommitted()) {
             createAuthCookie(httpResponse, "", getCookieDomain(),
-                             getCookiePath(), 0, isHttps
+                             getCookiePath(), 0, false, isHttps
             );
             // If response code is 401. Then WWW-Authenticate Header should be
             // present.. reset to 403 if not found..
